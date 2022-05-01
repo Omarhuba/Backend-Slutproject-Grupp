5bc7bd4bb3d0bedd6c328eef88943c766f9a255e
Omarhuba <omaruba@gmail.com>
2022-05-01 23:09:08 +0200

    little bug fixing

--- routes/router.js
+++ routes/router.js
@@ -4,11 +4,14 @@ const router = express.Router()
 
 const { register } = require('../controllers/auth/resisterControll')
 const { login } = require('../controllers/auth/loginControll')
+const { createTask } = require('../controllers/taskControllers')
 
 
 // Post requests
 router.post('/register', register)
 router.post('/login', login)
+router.post('/task', createTask)
+
 
 
 
