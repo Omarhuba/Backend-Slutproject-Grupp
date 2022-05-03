499fd201c39608a6d9eacf4f6173e9ae75fad677
Omarhuba <omaruba@gmail.com>
2022-05-03 14:31:59 +0200

    fixt dynamisc worker:_id

--- controllers/messageControll.js
+++ controllers/messageControll.js
@@ -9,17 +9,17 @@ require('../database/connection')
 
 const createMessage = async (req, res) => {
     try {
-        const { title, email, content } = req.body;
-        console.log('CQa ' + req.body.title)
-        const senders = await User.findOne({ email }).exec()
-        console.log('CQb ' + senders._id)
-
-        const tasks = await Task.findOne({ title}).exec()
-        console.log('CQc ' + tasks._id)
+        const { title, content } = req.body;
+        const token = req.header.token
+        const senders = await User.findOne({ token }).exec()
+            console.log('akjdhöksjfhaöskdj'+ senders);
+            const tasks = await Task.findOne({ title }).exec()
+            console.log(tasks);
+            console.log('task'+ tasks);
 
     const message =  await new Message({
         sender_id:senders._id,
-        task_id:tasks._id,
+        task_id:tasks._id.toString(),
         content
 
     })
@@ -30,7 +30,7 @@ const createMessage = async (req, res) => {
     } catch (error) {
       res.status(400).json(error.message);
     }
-};
+}
 
 
 const getMessageByTask = async (req, res, next) => {
